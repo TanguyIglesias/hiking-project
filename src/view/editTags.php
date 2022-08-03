46f5e52e047258df74ca8e398d0ad059975ea55c
TanguyIglesias <tanguyiglesias@gmail.com>
2022-08-03 14:23:18 +0200

    CheckBox Tag

--- src/view/editTags.php
+++ src/view/editTags.php
@@ -7,12 +7,19 @@ $tag = new Tag();
 $getTag = $tag->getTag();
 $tagInfo = $tag->linkTag($hike_id);
 $tagArr = array();
-array_push($tagArr,...$tagInfo);
+foreach ($tagInfo as $key => $value)
+{
+    array_push($tagArr,$tag->getTagById($value['tag_id']));
+}
 
 
 
 echo '<pre>';
-var_dump(...$tagArr);
+var_dump($tagArr);
+echo '</pre>';
+
+echo '<pre>';
+var_dump($hike_id);
 echo '</pre>';
 
 require_once '../view/head.php';
@@ -21,10 +28,17 @@ require_once '../view/head.php';
 
 <body>
 <?php require_once '../view/header.php';?>
-    <ol>
-        <?php foreach ( $getTag as $key => $value):  ?>
-            <li><p><?= $value['tag_name']?> </p></li>
-        <?php endforeach ?>
-    </ol>
+    <form method="POST" action="/editTag">
+            <?php foreach ( $getTag as $key => $value):  ?>
+                <input type="checkbox" value="<?= $value['tag_id']?>" name="tag_name[]" 
+                
+                
+                <?php foreach($tagArr as $key => $tag){
+                    if($tag["tag_name"] == $value['tag_name'])echo 'checked="checked"';}?>> 
+                      
+                    <?= $value['tag_name']?></br>    
+            <?php endforeach ?>
+        <button type="submit" name="editTag">Modifier</button>
+    </form>
 </body>
 
